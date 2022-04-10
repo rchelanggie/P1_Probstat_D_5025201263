58395483305f4b29bb1b97cb432babc11d5305f7
rchelanggie <94432091+rchelanggie@users.noreply.github.com>
2022-04-10 18:50:51 +0700

    Add files via upload

---
+++ Modul1_1.R
@@ -0,0 +1,28 @@
+#No 1. Penyurvei memilih acak orang sampai bertemu yang menghadiri acara sebelumnya
+#No 1a. Peluang penyurvei tidak menghadiri
+x = 3
+p = 0.20
+dgeom(x, p)
+
+#No 1b. Mean dengan 10000 random
+n = 10000
+prob = 0.20
+x = 3
+mean(rgeom(n, prob) == x)
+
+#No 1c. Perbandingan hasil a dan b
+# 1a -> hasilnya akan tetap sama 
+# 1b -> hasilnya berbeda-beda karena nilai yang diambil merupakan nilai acak
+
+#No 1d. Histogram peluang x=3 gagal sebelum sukses pertama
+set.seed(0)
+hist(rgeom(n, prob),
+  main="Histogram Distribusi Geometrik", 
+  xlim = c(0,30), xlab = "X",
+)
+
+#No 1e. Nilai rataan dan varian
+e1 <- (rataan = n/p)
+e2 <- (varian <- (n-p)/(p^2))
+e1
+e2
